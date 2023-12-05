ecf0a6491776296ff3ba25bdf2d2eae4d109d4e4
Alex D <152988659+lodger-close@users.noreply.github.com>
2023-12-05 20:58:25 +0700

    Create db.php

---
+++ db.php
@@ -0,0 +1,17 @@
+<?php
+// Database connection
+class Database {
+    private $conn;
+
+    public function __construct() {
+        $this->conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
+}
